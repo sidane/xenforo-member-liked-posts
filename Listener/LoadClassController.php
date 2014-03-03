73136d0e2444b22fa1580348ba91120f3a513872
Niall Mullally <niall@4l.ie>
2014-03-03 22:21:07 +0000

    Fix spacing

--- Listener/LoadClassController.php
+++ Listener/LoadClassController.php
@@ -2,19 +2,11 @@
 
 class Sidane_MemberLikedPosts_Listener_LoadClassController
 {
-    /**
-     * Instruct the system that XenForo_Controller_Member
-     * should be extended by Dev_Controller_Member
-     *
-     * @param string $class
-     * @param array $extend
-     */
-    public static function loadClassController($class, array &$extend)
+  public static function loadClassController($class, array &$extend)
+  {
+    if ($class == 'XenForo_ControllerPublic_Member')
     {
-        if ($class == 'XenForo_ControllerPublic_Member')
-        {
-            $extend[] = 'Sidane_MemberLikedPosts_ControllerPublic_Member';
-        }
+      $extend[] = 'Sidane_MemberLikedPosts_ControllerPublic_Member';
     }
+  }
 }
-
--- Listener/LoadClassModel.php
+++ Listener/LoadClassModel.php
@@ -2,18 +2,11 @@
 
 class Sidane_MemberLikedPosts_Listener_LoadClassModel
 {
-    /**
-     * Instruct the system that XenForo_Model_Member
-     * should be extended by Dev_Model_Member
-     *
-     * @param string $class
-     * @param array $extend
-     */
-    public static function loadClassModel($class, array &$extend)
+  public static function loadClassModel($class, array &$extend)
+  {
+    if ($class == 'XenForo_Model_Post')
     {
-        if ($class == 'XenForo_Model_Post')
-        {
-            $extend[] = 'Sidane_MemberLikedPosts_Model_Post';
-        }
+      $extend[] = 'Sidane_MemberLikedPosts_Model_Post';
     }
+  }
 }
